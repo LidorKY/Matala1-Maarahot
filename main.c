f72d96499d7c2aad2e04a2f03eb7af49acbdca7e
LidorKY <118388214+LidorKY@users.noreply.github.com>
2022-11-22 21:36:46 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -5,52 +5,45 @@ int main(){
 
 int x,y;
 
-printf("pls enter your first number: \n");
 scanf("%d",&x);
 while(x <= 0){
-printf("pls enter a number that is bigger than 0: \n");
 scanf("%d",&x);  
 }
 
-printf("pls enter your second number: \n");
 scanf("%d",&y);
 while(y < x){
-printf("pls enter a number that is bigger than the first number: \n");
 scanf("%d",&y); 
 }
 
 // printing armstrong nubers
-printf("The Armstrong numbers are: ");
+printf("\nThe Armstrong numbers are:");
 for(int i = x; i <= y; i++){
     if(isArmstrong(i)){
-        printf("%d ",i);
+        printf(" %d",i);
     }
 }
-printf("\n");
 
  //printing palindrome numbers
-printf("The Palindrome numbers are: ");
+printf("\nThe Palindrome numbers are:");
 for(int i = x; i <= y; i++){
     if(isPalindrome(i)){
-        printf("%d ",i);
+        printf(" %d",i);
     }
 }
-printf("\n");
 
 // printing prime numbers
-printf("The Prime numbers are: ");
+printf("\nThe Prime numbers are:");
 for(int i = x; i <= y; i++){
     if(isPrime(i)){
-        printf("%d ",i);
+        printf(" %d",i);
     }
 }
-printf("\n");
 
 //printing strong numbers
-printf("The Strong numbers are: ");
+printf("\nThe Strong numbers are:");
 for(int i = x; i <= y; i++){
     if(isStrong(i)){
-        printf("%d ",i);
+        printf(" %d",i);
     }
 }
 printf("\n");
